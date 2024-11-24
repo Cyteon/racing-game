13be4f3921df658670161312886c2ac2fc5f76ac
cyteon <cyteon>
2024-11-24 14:38:53 +0100

    tweak values + F to flip

--- scripts/car.gd
+++ scripts/car.gd
@@ -3,6 +3,10 @@ extends VehicleBody3D
 @export var ENGINE_POWER: int = 300
 @export var MAX_STEER: float = 0.4
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("flip"):
+		rotation = Vector3.ZERO
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if get_parent().run_started:
@@ -16,7 +20,6 @@ func _process(delta: float) -> void:
 			tween.tween_property($AudioStreamPlayer3D, "volume_db", -10, .5)
 	
 	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 10)
-
 
 func _on_audio_stream_player_3d_finished() -> void:
 	$AudioStreamPlayer3D.play()
