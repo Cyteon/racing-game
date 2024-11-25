59678d6c402e195b173015e9487badd0ebda3eaf
cyteon <cyteon>
2024-11-25 18:16:00 +0100

    pause menu

---
+++ scripts/reusables/track_canvas.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+var was_paused: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		get_tree().paused = false
+		$Control/PauseScreen.visible = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause") and was_paused:
+		get_tree().paused = false
+		$Control/PauseScreen.visible = false
+	
+	was_paused = get_tree().paused
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	$Control/PauseScreen.visible = false
+
+
+func _on_return_to_menu_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/menus/MainMenu.tscn")
+
+
+func _on_play_again_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
--- scripts/track.gd
+++ scripts/track.gd
@@ -19,6 +19,10 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("reset"):
 		get_tree().reload_current_scene()
+	
+	if event.is_action_pressed("pause"):
+		get_tree().paused = true
+		$CanvasLayer/Control/PauseScreen.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
